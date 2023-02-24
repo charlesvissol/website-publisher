1ce8e61d887b7721b08501dce2a22a2b70ccae4e
vissol <charles.vissol@protonmail.com>
2023-02-24 12:22:14 +0100

    PDF refactoring

--- src/main/java/org/angrybee/website/publish/impl/PublisherPdf.java
+++ src/main/java/org/angrybee/website/publish/impl/PublisherPdf.java
@@ -23,7 +23,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.PipedOutputStream;
 import java.net.URISyntaxException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
