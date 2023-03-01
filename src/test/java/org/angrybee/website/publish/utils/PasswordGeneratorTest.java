951c5faafdab63f23d28ea66564409242b56dc95
vissol <charles.vissol@protonmail.com>
2023-03-01 23:32:20 +0100

    unit test

--- src/test/java/org/angrybee/website/publish/utils/PasswordGeneratorTest.java
+++ src/test/java/org/angrybee/website/publish/utils/PasswordGeneratorTest.java
@@ -2,7 +2,6 @@ package org.angrybee.website.publish.utils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
